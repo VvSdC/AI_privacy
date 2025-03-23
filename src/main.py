34e9d6bb179809025243edb3462cd5f9180659a7
VvSdC <vvsdcharan2001@gmail.com>
2025-03-23 11:50:22 +0530

    Integrated FastAPI for API testing

--- src/main.py
+++ src/main.py
@@ -1,43 +1,5 @@
-# from service.presidioAnalyzer import PresidioAnalyzer
-# from service.locationRecognizer import LocationRecognizer
-# from service.creditCardRecognizer import CreditCardRecognizer
-from service.starpiiAnalyzer import StarPIIAnalyzer
+import uvicorn
+from routes.router import app
 
-dummy_text = """
-John's contact number in the US is +1 (415) 555-2671, while his UK number is +44 7911 123456.
-For India, reach out at +91-9876543210 or 98765 43210.
-
-Some random IP addresses: 
-- IPv4: 192.168.1.1, 10.0.0.1, 255.255.255.255, 172.16.32.5
-- Invalid IPv4: 999.999.999.999, 300.300.300.300
-- IPv6: 2001:0db8:85a3:0000:0000:8a2e:0370:7334, ::1, fe80::1ff:fe23:4567:890a
-- Invalid IPv6: 12345::6789, 2001:db8:::1
-
-Locations mentioned:
-- New York, USA
-- London, United Kingdom
-- Bangalore, India
-- San Francisco, California
-- Sydney, Australia
-- Fake Location: Atlantis, Moon Base Alpha and London and also loNdon
-
-
-Credit Card Numbers:
-- Visa 4111 1111 1111 1111
-- MasterCard 5500-0000-0000-0004
-- Discover  6011 0000 0000 0004
-- AMEX 3714 496353 98431
-- NO spaces  4111111111111111  6011000000000004
-- 4111 1111 1111 1111
-
-"""
-
-# presidio_analyzer = PresidioAnalyzer()
-# print(presidio_analyzer.analyze(dummy_text))
-# results = CreditCardRecognizer.analyze(dummy_text)
-
-analyzer = StarPIIAnalyzer()
-results = analyzer.analyze(dummy_text)
-
-for result in results:
-    print(f"{dummy_text[result.start:result.end]} -> {result.entity_type}")
+if __name__ == "__main__":
+    uvicorn.run("main:app",host="127.0.0.1",port=3081,reload=True)
--- src/service/analyzer.py
+++ src/service/analyzer.py
@@ -1,8 +1,7 @@
-from service.creditCardRecognizer import CreditCardRecognizer
-from service.locationRecognizer import LocationRecognizer
-from service.phoneNumberRecognizer import PhoneNumberRecognizer
+from recognizers.creditCardRecognizer import CreditCardRecognizer
+from recognizers.locationRecognizer import LocationRecognizer
+from recognizers.phoneNumberRecognizer import PhoneNumberRecognizer
 from service.starpiiAnalyzer import StarPIIAnalyzer
-from service.ipAddressRecognizer import IpAddressRecognizer
 
 class Analyzer:
     def __init__(self):
@@ -14,7 +13,15 @@ class Analyzer:
         results.extend(LocationRecognizer.analyze(text))
         results.extend(PhoneNumberRecognizer.analyze(text))
         results.extend(CreditCardRecognizer.analyze(text))
-        results.extend(IpAddressRecognizer.analyze(text))
-        return results
+
+        entity_wise_words = dict()
+        for result in results:
+            if result.entity_type in entity_wise_words:
+                entity_wise_words[result.entity_type].append(text[result.start:result.end])
+            else:
+                entity_wise_words[result.entity_type] = [text[result.start:result.end]]
+
+        return entity_wise_words
+        
 
         
